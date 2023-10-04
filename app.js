f53767c6ed3035cc0d1348a47b064411272a3f25
Joshua Smith <ws.joshua@gmail.com>
2023-10-04 08:13:18 -0600

    update whitespace for lesson

--- app.js
+++ app.js
@@ -1,40 +1,77 @@
 // Task 1 - Notes for JS Evaluation Predictions
 
+// List 3 expressions that surprised you or gave unexpected results, why?
+
+// List 2 exprsssions you felt comfortable interpreting at a glance.
+
+// List 1 expression you are still 'shaky' on, why?
+
+
+
+
+
 // Task 2 - Practice JS Basics
 // 1 - 
+
 // 2 - 
+
 // 3 - 
+
 // 4 - 
+
 // 5 - 
+
 // 6 - 
+
 // 7 - 
+
 // 8 - 
+
 // 9 -
+
 // 10 - 
 
 
 //// Task 3 - Conditionals Practice 
 // 1 - 
+
 // 2 - 
+
 // 3 - 
+
 // 4 - 
+
 // 5 - 
+
 // 6 - 
+
 // 7 - 
+
 // 8 - 
+
 // 9 -
+
 // 10 - 
 
 //// Task 4 - Conditionals Muscle Memory
 // 1 - 
+
 // 2 - 
+
 // 3 - 
+
 // 4 - 
+
 // 5 - 
+
 // 6 - 
+
 // 7 - 
+
 // 8 - 
+
 // 9 -
+
 // 10 - 
 
 
