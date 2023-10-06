c1bc09187cdfe5d61878d8e72afe71ea14d8688c
maker-jws <ws.joshua@gmail.com>
2023-10-05 22:47:43 -0600

    update exercise 4 test

--- challenge-problems/test/index.js
+++ challenge-problems/test/index.js
@@ -46,8 +46,6 @@ const {
 //   })
 // });
 
-
-
 // describe("challenge 3 - isPrime", function () {
 //   it("should return an boolean", function(){
 //     expect(isPrime(10),'10').to.be.false
@@ -63,7 +61,7 @@ const {
 //   })
 //   it(`should return false for negative values or non-integer numbers`, function(){
 //     expect(isPrime(3.14), `error for decimal 3.14`).to.be.false
-//     expect(isPrime(7.0), `error for decimal 7.0`).to.be.true 
+//     expect(isPrime(7.0), `error for decimal 7.0`).to.be.true
 //     expect(isPrime(7.2), `error for decimal 7.2`).to.be.false
 //     expect(isPrime(-37)).to.be.false
 //     expect(isPrime(NaN)).to.be.false
@@ -71,25 +69,39 @@ const {
 
 // });
 
-// describe("challenge 3 - isPrime", function () {
-//   it("should return an boolean", function(){
-//     expect(isPrime(10),'10').to.be.false
-//     expect(isPrime(2017),'2017').to.be.true
-//     expect(isPrime(),'null').to.be.false
-//   })
-//   it("should correctly identify prime values", function(){
-//     expect(isPrime(2)).equals(true)
-//     expect(isPrime(10)).equals(false)
-//     expect(isPrime(11)).equals(true)
-//     expect(isPrime(9)).equals(false)
-//     expect(isPrime(2017)).equals(true)
-//   })
-//   it(`should return false for negative values or non-integer numbers`, function(){
-//     expect(isPrime(3.14), `error for decimal 3.14`).to.be.false
-//     expect(isPrime(7.0), `error for decimal 7.0`).to.be.true 
-//     expect(isPrime(7.2), `error for decimal 7.2`).to.be.false
-//     expect(isPrime(-37)).to.be.false
-//     expect(isPrime(NaN)).to.be.false
-//   })
+// const cat1 = catBuilder('Garfield', 'golden', ['scratching-post', 'yarn']);
+// cat1; // => { name: 'Garfield', color: 'golden', toys: ['scratching-post', 'yarn'] }
 
-// });
+// const cat2 = catBuilder('Whiskers', 'rainbow', ['poptarts']);
+// cat2; // => { name: 'Whiskers', color: 'rainbow', toys: [ 'poptarts' ] }
+
+describe("challenge 4 - catBuilder", function () {
+  const testcase = catBuilder("Garfield", "golden", [
+    "scratching-post",
+    "yarn",
+  ]);
+  it("should return an object", function () {
+    expect(testcase).to.be.an("object");
+  });
+  it("should include object keys name, color, and toys", function () {
+    expect(testcase).to.have.all.keys("color", "toys", "name");
+    expect(testcase).to.not.have.keys("pizza", "cat");
+  });
+  it("should include a 'toys' key storing an array", function () {
+    expect(testcase.toys).to.be.an("array");
+  });
+  it(`should return correct key/value pairs`, function () {
+    expect(
+      catBuilder("Garfield", "golden", ["scratching-post", "yarn"])
+    ).to.deep.equal({
+      name: "Garfield",
+      color: "golden",
+      toys: ["scratching-post", "yarn"],
+    });
+    expect(catBuilder("Whiskers", "rainbow", ["poptarts"])).to.deep.equal({
+      name: "Whiskers",
+      color: "rainbow",
+      toys: ["poptarts"],
+    });
+  });
+});
